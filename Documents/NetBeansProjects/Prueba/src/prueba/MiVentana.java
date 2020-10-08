eceda2efd0120a7f30ddaad4165495d1f4a5fb97
Fernando Hernandez <fernando1999jw@gmail.com>
2020-10-07 23:42:14 -0500

    1.1

---
+++ Documents/NetBeansProjects/Prueba/src/prueba/MiVentana.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Fer
+ */
+public class MiVentana extends JFrame{
+    
+    public MiVentana(){
+        super("Mi otra ventana");
+        setSize(400,300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+}
