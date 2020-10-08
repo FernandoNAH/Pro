1ec42a04385e44b79f7b2001bb32bdc4a6c43860
Fernando Hernandez <fernando1999jw@gmail.com>
2020-10-07 23:50:38 -0500

    2.0

--- Documents/NetBeansProjects/Prueba/src/prueba/MiVentana.java
+++ Documents/NetBeansProjects/Prueba/src/prueba/MiVentana.java
@@ -1,6 +1,8 @@
 package prueba;
 
-import javax.swing.JFrame;
+import java.awt.Container;
+import java.awt.FlowLayout;
+import javax.swing.*;
 
 /**
  *
@@ -10,8 +12,18 @@ public class MiVentana extends JFrame {
 
     public MiVentana() {
         super("Mi otra ventana");
-        setSize(400, 300);
+        setSize(500, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Container cp = getContentPane();
+        cp.setLayout(new FlowLayout());
+        JLabel etiqueta = new  JLabel("Nombre:");
+        JTextField texto = new JTextField(20);
+        JButton boton = new JButton("edsd");
+        cp.add(etiqueta);
+        cp.add(texto);
+        cp.add(boton);
+        
+        
     }
 
 }
